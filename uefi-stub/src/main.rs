c0fe2f0e01ec1ae94c4d0243c92047607a2ed3d7
Jarl Evanson <evanson.jarl@gmail.com>
2024-08-25 22:12:40 -0500

    Ensure stdout and stderr are setup; print startup

--- uefi-stub/src/main.rs
+++ uefi-stub/src/main.rs
@@ -3,13 +3,55 @@
 #![no_std]
 #![no_main]
 
-#[export_name = "efi_main"]
-pub extern "efiapi" fn main() {
+use core::fmt::Write;
+
+use uefi::{
+    proto::console::text,
+    system::{with_stderr, with_stdout},
+    Status,
+};
+
+/// The name of this bootloader.
+const BOOTLOADER_NAME: &str = "capora-boot-stub";
+/// The version of capora-boot-stub.
+const BOOTLOADER_VERSION: &str = core::env!("CARGO_PKG_VERSION");
+
+#[uefi::entry]
+fn main() -> Status {
+    with_stdout(setup_output);
+    with_stderr(setup_output);
+
+    let _ =
+        with_stdout(|stdout| writeln!(stdout, "Booting {BOOTLOADER_NAME} {BOOTLOADER_VERSION}"));
+
     loop {}
 }
 
-#[panic_handler]
-fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+pub fn setup_output(output: &mut text::Output) {
+    let current_mode = output.current_mode();
+    match current_mode {
+        Ok(None) => {
+            let mode = output
+                .modes()
+                .max_by(|mode_0, mode_1| mode_0.rows().cmp(&mode_1.rows()))
+                .expect("according to UEFI specification, at least one mode must be supported");
+
+            // Just ignore any errors, we can't report them and we shouldn't not continue due to
+            // missing output.
+            let _ = output.set_mode(mode);
+        }
+        Ok(Some(_)) | Err(_) => {}
+    }
+}
+
+#[cfg_attr(not(test), panic_handler)]
+#[cfg_attr(test, expect(dead_code))]
+fn panic_handler(info: &core::panic::PanicInfo) -> ! {
+    if uefi::table::system_table_boot().is_some() {
+        let _ = with_stderr(|stderr| writeln!(stderr, "{info}"));
+        let _ = with_stdout(|stderr| writeln!(stderr, "{info}"));
+    }
+
     loop {
         core::hint::spin_loop()
     }
