519e3e29b7f8c2b32c54a3e3b4ccbc90e658ad87
Jarl Evanson <evanson.jarl@gmail.com>
2024-08-30 07:33:35 -0500

    Refactor `xtask` to seperate CLI parsing

---
+++ xtask/src/cli.rs
@@ -0,0 +1,154 @@
+//! Command line parsing and command construction.
+
+use std::path::PathBuf;
+
+/// The action to carry out.
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub enum Action {
+    /// Build capora-boot-stub.
+    Build {
+        /// The [`Arch`] for which capora-boot-stub should be built.
+        arch: Arch,
+        /// Whether capora-boot-stub should be built in release mode.
+        release: bool,
+    },
+    /// Runs capora-boot-stub.
+    Run {
+        /// The [`Arch`] for which capora-boot-stub should be built.
+        arch: Arch,
+        /// Whether capora-boot-stub should be built in release mode.
+        release: bool,
+        /// The path to the OVMF code file used to run UEFI.
+        ovmf_code: PathBuf,
+        /// The path to the OVMF vars file used to run UEFI.
+        ovmf_vars: PathBuf,
+    },
+}
+
+/// Parses arguments to construct an [`Action`].
+pub fn parse_arguments() -> Action {
+    let mut matches = command_parser().get_matches();
+    let (subcommand_name, subcommand_matches) =
+        matches.remove_subcommand().expect("subcommand required");
+    match subcommand_name.as_str() {
+        "build" => parse_build_arguments(subcommand_matches),
+        "run" => parse_run_arguments(subcommand_matches),
+        name => unreachable!("unexpected subcommand {name:?}"),
+    }
+}
+
+/// Parses subcommand arguments for the [`Action::Build`] subcommand.
+pub fn parse_build_arguments(mut matches: clap::ArgMatches) -> Action {
+    let arch = matches
+        .remove_one::<Arch>("arch")
+        .expect("arch is a required argument");
+    let release = matches.remove_one::<bool>("release").unwrap_or(false);
+
+    Action::Build { arch, release }
+}
+
+/// Parses subcommand arguments for the [`Action::Run`] subcommand.
+pub fn parse_run_arguments(mut matches: clap::ArgMatches) -> Action {
+    let arch = matches
+        .remove_one::<Arch>("arch")
+        .expect("arch is a required argument");
+    let release = matches.remove_one::<bool>("release").unwrap_or(false);
+
+    let ovmf_code = matches
+        .remove_one::<PathBuf>("ovmf-code")
+        .expect("ovmf-code is required");
+    let ovmf_vars = matches
+        .remove_one::<PathBuf>("ovmf-vars")
+        .expect("ovmf-vars is required");
+
+    Action::Run {
+        arch,
+        release,
+        ovmf_code,
+        ovmf_vars,
+    }
+}
+
+/// Returns the clap command parser.
+pub fn command_parser() -> clap::Command {
+    let arch_arg = clap::Arg::new("arch")
+        .long("arch")
+        .value_parser(clap::builder::EnumValueParser::<Arch>::new())
+        .required(true);
+
+    let release_arg = clap::Arg::new("release")
+        .help("Build capora-boot-stub in release mode")
+        .long("release")
+        .short('r')
+        .action(clap::ArgAction::SetTrue);
+
+    let build_subcommand = clap::Command::new("build")
+        .about("Builds capora-boot-stub")
+        .arg(
+            arch_arg
+                .clone()
+                .help("The architecture for which capora-boot-stub should be built"),
+        )
+        .arg(release_arg.clone());
+
+    let run_subcommand = clap::Command::new("run")
+        .about("Runs capora-boot-stub")
+        .arg(arch_arg.help("The architecture for which capora-boot-stub should be built and run"))
+        .arg(release_arg)
+        .arg(
+            clap::Arg::new("ovmf-code")
+                .long("ovmf-code")
+                .short('c')
+                .value_parser(clap::builder::PathBufValueParser::new())
+                .required(true),
+        )
+        .arg(
+            clap::Arg::new("ovmf-vars")
+                .long("ovmf-vars")
+                .short('v')
+                .value_parser(clap::builder::PathBufValueParser::new())
+                .required(true),
+        );
+
+    clap::Command::new("xtask")
+        .about("Developer utility for running various tasks in capora-boot-stub")
+        .subcommand(build_subcommand)
+        .subcommand(run_subcommand)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+}
+
+/// The architectures supported by capora-boot-stub.
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum Arch {
+    /// The `x86_64` architecture.
+    X86_64,
+}
+
+impl Arch {
+    /// Returns the [`Arch`] as its rustc target triple.
+    pub fn as_target_triple(&self) -> &'static str {
+        match self {
+            Self::X86_64 => "x86_64-unknown-uefi",
+        }
+    }
+
+    /// Returns the [`Arch`] as its textual representation.
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::X86_64 => "x86_64",
+        }
+    }
+}
+
+impl clap::ValueEnum for Arch {
+    fn value_variants<'a>() -> &'a [Self] {
+        static ARCHES: &[Arch] = &[Arch::X86_64];
+
+        ARCHES
+    }
+
+    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
+        Some(clap::builder::PossibleValue::new(self.as_str()))
+    }
+}
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,77 +1,126 @@
 //! Helper crate for building and testing capora boot stub.
 
-use std::{env, ffi::OsString, path::PathBuf};
+use core::fmt;
+use std::{ffi::OsString, path::PathBuf};
 
-fn main() -> Result<(), ()> {
-    let subcommand = parse_arguments(env::args_os());
+use cli::{parse_arguments, Action, Arch};
 
-    match subcommand {
-        Subcommand::Build { release, target } => build(target, release),
-        Subcommand::Run {
+pub mod cli;
+
+fn main() {
+    match parse_arguments() {
+        Action::Build { arch, release } => match build(arch, release) {
+            Ok(path) => println!("kernel located at \"{}\"", path.display()),
+            Err(error) => eprintln!("error while building kernel: {error}"),
+        },
+        Action::Run {
+            arch,
             release,
-            target,
             ovmf_code,
             ovmf_vars,
-        } => run(target, release, ovmf_code, ovmf_vars),
-    }
+        } => match run(arch, release, ovmf_code, ovmf_vars) {
+            Ok(()) => {}
+            Err(error) => eprintln!("error while running kernel: {error}"),
+        },
+    };
 }
 
-/// Builds capora uefi stub with the specified target and release mode.
-pub fn build(target: Target, release: bool) -> Result<(), ()> {
+/// Builds capora-boot-stub.
+pub fn build(arch: Arch, release: bool) -> Result<PathBuf, BuildError> {
     let mut cmd = std::process::Command::new("cargo");
     cmd.arg("build");
-    cmd.args(["--package", "uefi-stub"]);
+    cmd.args(["--package", "boot-stub"]);
 
-    cmd.args(["--target", target.as_triple()]);
-
+    cmd.args(["--target", arch.as_target_triple()]);
     if release {
         cmd.arg("--release");
     }
 
-    cmd.status().map(|_| ()).map_err(|_| ())
+    let mut binary_location = PathBuf::with_capacity(50);
+    binary_location.push("target");
+    binary_location.push(arch.as_target_triple());
+    if release {
+        binary_location.push("release");
+    } else {
+        binary_location.push("debug");
+    }
+    binary_location.push("boot-stub.efi");
+
+    let status = cmd.status()?;
+    if !status.success() {
+        return Err(BuildError::UnsuccessfulBuild {
+            code: status.code(),
+        });
+    }
+
+    Ok(binary_location)
 }
 
-/// Builds capora uefi stub with the specified target and release mode, then executes the
-/// uefi stub for testing purposes.
+/// Various errors that can occur while building capora-boot-stub.
+#[derive(Debug)]
+pub enum BuildError {
+    /// An error occurred while launching the process.
+    ProcessError(std::io::Error),
+    /// The build was unsuccessful.
+    UnsuccessfulBuild {
+        /// The exit code of the child process that was launched.
+        code: Option<i32>,
+    },
+}
+
+impl From<std::io::Error> for BuildError {
+    fn from(value: std::io::Error) -> Self {
+        Self::ProcessError(value)
+    }
+}
+
+impl fmt::Display for BuildError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::ProcessError(error) => writeln!(f, "error while launching cargo: {error}"),
+            Self::UnsuccessfulBuild { code: Some(code) } => {
+                writeln!(f, "cargo failed with exit status {code}")
+            }
+            Self::UnsuccessfulBuild { code: None } => f.write_str("cargo terminated by signal"),
+        }
+    }
+}
+
+/// Builds and runs capora-boot-stub.
 pub fn run(
-    target: Target,
+    arch: Arch,
     release: bool,
     ovmf_code: PathBuf,
     ovmf_vars: PathBuf,
-) -> Result<(), ()> {
-    let mut cmd = std::process::Command::new("cargo");
-    cmd.arg("build");
-    cmd.args(["--package", "uefi-stub"]);
+) -> Result<(), RunError> {
+    let stub_path = build(arch, release)?;
+    let fat_directory =
+        build_fat_directory(arch, stub_path).map_err(RunError::BuildFatDirectoryError)?;
 
-    cmd.args(["--target", target.as_triple()]);
-
-    if release {
-        cmd.arg("--release");
-    }
-
-    cmd.status().map_err(|_| ())?;
-
-    let qemu_name = match target {
-        Target::X86_64 => "qemu-system-x86_64",
+    let qemu_name = match arch {
+        Arch::X86_64 => "qemu-system-x86_64",
     };
+
     let mut cmd = std::process::Command::new(qemu_name);
 
-    // Disable necessary devices.
+    // Disable unnecessary devices.
     cmd.arg("-nodefaults");
 
     cmd.args(["-boot", "menu=on,splash-time=0"]);
-    match target {
-        Target::X86_64 => {
-            // Use a fairly modern machine as the emulation target.
+    match arch {
+        Arch::X86_64 => {
+            // Use a fairly modern machine to target.
             cmd.args(["-machine", "q35"]);
 
-            // Allocate memory to emulator.
+            // Allocate some memory.
             cmd.args(["-m", "256M"]);
 
-            // Setup graphics
+            // Use vga graphics.
             cmd.args(["-vga", "std"]);
 
-            cmd.arg("--enable-kvm");
+            if std::env::consts::OS == "linux" {
+                cmd.arg("-enable-kvm");
+            }
         }
     }
 
@@ -79,198 +128,88 @@ pub fn run(
     ovmf_code_arg.push(ovmf_code);
     cmd.arg("-drive").arg(ovmf_code_arg);
 
-    let mut ovmf_vars_arg = OsString::from("if=pflash,format=raw,readonly=off,file=");
+    let mut ovmf_vars_arg = OsString::from("if=pflash,format=raw,readonly=on,file=");
     ovmf_vars_arg.push(ovmf_vars);
     cmd.arg("-drive").arg(ovmf_vars_arg);
 
     let mut fat_drive_arg = OsString::from("format=raw,file=fat:rw:");
-    fat_drive_arg.push(build_fat_directory(release, target).unwrap());
+    fat_drive_arg.push(fat_directory);
     cmd.arg("-drive").arg(fat_drive_arg);
 
-    cmd.status().map(|_| ()).map_err(|_| ())
+    let status = cmd.status()?;
+    if !status.success() {
+        return Err(RunError::QemuError {
+            code: status.code(),
+        });
+    }
+
+    Ok(())
 }
 
-/// Setups the boot directory.
-pub fn build_fat_directory(release: bool, target: Target) -> Result<PathBuf, std::io::Error> {
-    let build_mode = if release { "release" } else { "debug" };
+/// Various errors that can occur while building and running capora-boot-stub.
+#[derive(Debug)]
+pub enum RunError {
+    /// An error occurred while building the kernel.
+    BuildError(BuildError),
+    /// An error occurred while building the fat directory.
+    BuildFatDirectoryError(std::io::Error),
+    /// An error ocurred while launching qemu.
+    ProcessError(std::io::Error),
+    /// QEMU exited with a non-zero exit code.
+    QemuError {
+        /// The exit code of QEMU.
+        code: Option<i32>,
+    },
+}
 
-    let build_directory = PathBuf::from("target")
-        .join(target.as_triple())
-        .join(build_mode);
+impl From<BuildError> for RunError {
+    fn from(value: BuildError) -> Self {
+        Self::BuildError(value)
+    }
+}
 
-    let fat_directory = PathBuf::from("run")
-        .join(target.as_str())
-        .join("fat_directory");
-    let boot_directory = fat_directory.join("EFI").join("BOOT");
+impl From<std::io::Error> for RunError {
+    fn from(value: std::io::Error) -> Self {
+        Self::ProcessError(value)
+    }
+}
+
+impl fmt::Display for RunError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::BuildError(error) => {
+                writeln!(f, "error while building capora-boot-stub: {error}")
+            }
+            Self::BuildFatDirectoryError(error) => {
+                writeln!(f, "error while building FAT directory: {error}")
+            }
+            Self::ProcessError(error) => writeln!(f, "error while launching QEMU: {error}"),
+            Self::QemuError { code: Some(code) } => {
+                writeln!(f, "qemu failed with exit status {code}")
+            }
+            Self::QemuError { code: None } => f.write_str("qemu terminated by signal"),
+        }
+    }
+}
+
+/// Builds the FAT directory structure used for running capora-boot-stub.
+pub fn build_fat_directory(arch: Arch, stub_path: PathBuf) -> Result<PathBuf, std::io::Error> {
+    let mut fat_directory = PathBuf::with_capacity(50);
+    fat_directory.push("run");
+    fat_directory.push(arch.as_str());
+    fat_directory.push("fat_directory");
+
+    let mut boot_directory = fat_directory.join("EFI");
+    boot_directory.push("BOOT");
     if !boot_directory.exists() {
         std::fs::create_dir_all(&boot_directory)?;
     }
 
-    let boot_file_name = match target {
-        Target::X86_64 => "BOOTX64.EFI",
+    let boot_file_name = match arch {
+        Arch::X86_64 => "BOOTX64.EFI",
     };
 
-    std::fs::copy(
-        build_directory.join("uefi-stub.efi"),
-        boot_directory.join(boot_file_name),
-    )?;
+    std::fs::copy(stub_path, boot_directory.join(boot_file_name))?;
 
     Ok(fat_directory)
-}
-
-/// The subcommand to execute.
-pub enum Subcommand {
-    /// Build
-    Build {
-        /// Indicates that capora uefi stub should be built in release mode.
-        release: bool,
-        /// Indicates the architecture of capora uefi stub to be built.
-        target: Target,
-    },
-    /// Run capora uefi stub.
-    Run {
-        /// Indicates the capora uefi stub should be built in release mode.
-        release: bool,
-        /// Indicates the architecture to build capora uefi stub and run the tests on.
-        target: Target,
-        /// The path to the OVMF code file used to run UEFI.
-        ovmf_code: PathBuf,
-        /// The path to the OVMF vars file used to run UEFI.
-        ovmf_vars: PathBuf,
-    },
-}
-
-/// The architectures supported by the kernel.
-#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
-pub enum Target {
-    /// The `x86_64` architecture.
-    X86_64,
-}
-
-impl Target {
-    /// Converts the given [`Target`] into its Rust target triple.
-    pub fn as_triple(self) -> &'static str {
-        match self {
-            Self::X86_64 => "x86_64-unknown-uefi",
-        }
-    }
-
-    /// Converts the given [`Target`] to its unique selector name.
-    pub fn as_str(self) -> &'static str {
-        match self {
-            Self::X86_64 => "x86_64",
-        }
-    }
-}
-
-impl clap::ValueEnum for Target {
-    fn value_variants<'a>() -> &'a [Self] {
-        static VALUES: &[Target] = &[Target::X86_64];
-
-        VALUES
-    }
-
-    fn to_possible_value(&self) -> Option<clap::builder::PossibleValue> {
-        Some(clap::builder::PossibleValue::new(self.as_str()))
-    }
-}
-
-/// Parses the given arguments and constructs a [`Subcommand`].
-pub fn parse_arguments(arguments: env::ArgsOs) -> Subcommand {
-    let argument_matches = command_parser().get_matches_from(arguments);
-    let (subcommand_name, subcommand_args) = argument_matches
-        .subcommand()
-        .expect("subcommand is required");
-
-    match subcommand_name {
-        "build" => parse_build_arguments(subcommand_args),
-        "run" => parse_run_arguments(subcommand_args),
-        other => unreachable!("unexpected subcommand: {other}"),
-    }
-}
-
-/// Parses the arguments to the build subcommand and constructs its [`Subcommand`].
-pub fn parse_build_arguments(subcommand_args: &clap::ArgMatches) -> Subcommand {
-    let release = subcommand_args
-        .get_one::<bool>("release")
-        .copied()
-        .unwrap_or(false);
-    let target = subcommand_args
-        .get_one::<Target>("target")
-        .copied()
-        .expect("target is required");
-
-    Subcommand::Build { release, target }
-}
-
-/// Parses the arguments to the run subcommand and constructs its [`Subcommand::Run`].
-pub fn parse_run_arguments(subcommand_args: &clap::ArgMatches) -> Subcommand {
-    let release = subcommand_args
-        .get_one::<bool>("release")
-        .copied()
-        .unwrap_or(false);
-    let target = subcommand_args
-        .get_one::<Target>("target")
-        .copied()
-        .expect("target is required");
-    let ovmf_code = subcommand_args
-        .get_one("ovmf-code")
-        .cloned()
-        .expect("ovmf-code is required");
-    let ovmf_vars = subcommand_args
-        .get_one("ovmf-vars")
-        .cloned()
-        .expect("ovmf-vars is required");
-
-    Subcommand::Run {
-        release,
-        target,
-        ovmf_code,
-        ovmf_vars,
-    }
-}
-
-/// Returns the clap command parser.
-pub fn command_parser() -> clap::Command {
-    let release_argument = clap::Arg::new("release")
-        .help("Builds the capora kernel in release mode")
-        .short('r')
-        .long("release")
-        .action(clap::ArgAction::SetTrue);
-
-    let target_argument = clap::Arg::new("target")
-        .long("target")
-        .value_parser(clap::builder::EnumValueParser::<Target>::new())
-        .required(true);
-
-    let build_subcommand = clap::Command::new("build")
-        .about("Build the capora kernel")
-        .arg(release_argument.clone())
-        .arg(target_argument.clone());
-
-    let run_subcommand = clap::Command::new("run")
-        .about("build capora uefi stub for in testing mode and run it in QEMU")
-        .arg(release_argument)
-        .arg(target_argument)
-        .arg(
-            clap::Arg::new("ovmf-code")
-                .long("ovmf-code")
-                .short('c')
-                .value_parser(clap::builder::PathBufValueParser::new())
-                .required(true),
-        )
-        .arg(
-            clap::Arg::new("ovmf-vars")
-                .long("ovmf-vars")
-                .short('v')
-                .value_parser(clap::builder::PathBufValueParser::new())
-                .required(true),
-        );
-
-    clap::Command::new("xtask")
-        .about("Developer utility for running various tasks in capora-kernel")
-        .subcommand(build_subcommand)
-        .subcommand(run_subcommand)
-        .subcommand_required(true)
-        .arg_required_else_help(true)
 }
