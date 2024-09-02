1de7da0073fcadab06f619f19abef02477dacf4b
Jarl Evanson <evanson.jarl@gmail.com>
2024-09-01 23:50:29 -0500

    Add additional actions to `test-application`
    These additions are done to create relocation entries, so that the
    ability of `capora-boot-stub` to execute relocation entries can be
    tested.

--- test-application/src/main.rs
+++ test-application/src/main.rs
@@ -3,9 +3,14 @@
 #![no_std]
 #![no_main]
 
+#[export_name = "SYMBOL"]
+static mut K: u8 = 0;
+
 /// The entry point of the test application.
 #[export_name = "_start"]
 pub fn entry() -> ! {
+    unsafe { core::ptr::write_volatile(core::ptr::addr_of_mut!(K), 2) }
+
     loop {}
 }
 
