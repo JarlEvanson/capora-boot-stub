80b9be564eb4d2f49e8477b21ee2b6ba0b4f296c
Jarl Evanson <evanson.jarl@gmail.com>
2024-09-02 19:38:58 -0500

    Add allocation for stack, context switch and GDT

--- boot-stub/src/main.rs
+++ boot-stub/src/main.rs
@@ -9,13 +9,14 @@ use core::{
     arch::x86_64,
     error,
     fmt::{self, Write},
+    mem,
 };
 
 use configuration::parse_and_interprete_configuration;
 use load_application::load_application;
-use mapper::VirtualMemoryMap;
+use mapper::{FrameRange, PageRange, VirtualMemoryMap, VirtualMemoryMapEntry};
 use uefi::{
-    boot,
+    boot::{self, AllocateType, MemoryType},
     proto::console::text,
     system::{with_stderr, with_stdout},
     Status,
@@ -42,6 +43,8 @@ const APPLICATION_REGION_SIZE: u64 = 0x0000000080000000;
 
 /// The maximum number of retries for finding a elf base before giving up.
 const BASE_RETRY_COUNT: u64 = 1000;
+
+const LOADED_STACK_SIZE: u64 = 64 * 1024;
 
 #[uefi::entry]
 fn main() -> Status {
@@ -79,7 +82,16 @@ fn main() -> Status {
         return Status::LOAD_ERROR;
     }
 
-    let mmeory_map = unsafe { boot::exit_boot_services(boot::MemoryType::LOADER_DATA) };
+    let (stack, gdt, context_switch) = match setup_general_mappings(&mut virtual_map) {
+        Ok(result) => result,
+        Err(error) => {
+            let _ = with_stderr(|stderr| writeln!(stderr, "{error}"));
+            let _ = with_stdout(|stdout| writeln!(stdout, "{error}"));
+            boot::stall(STALL_ON_ERROR_TIME);
+            return Status::LOAD_ERROR;
+        }
+    };
+    let memery_map = unsafe { boot::exit_boot_services(boot::MemoryType::LOADER_DATA) };
 
     // Already checked that the required bits are supported.
     let _ = set_required_bits();
@@ -158,6 +170,151 @@ impl fmt::Display for UnsupportedFeaturesError {
 }
 
 impl error::Error for UnsupportedFeaturesError {}
+
+const CONTEXT_STUB_BYTES: [u8; 13] = [
+    0x48, 0x31, 0xed, // xor rbp, rbp
+    0x0f, 0x22, 0xd8, // mov cr3, rax
+    0x48, 0x89, 0xcc, // mov rsp, rcx
+    0x6a, 0x00, // push 0x0
+    0xff, 0xe2, // jmp rdx
+];
+
+const GDT: [u64; 3] = [
+    // Null GDT entry.
+    0x00_0_0_00_000000_0000,
+    // Kernel code entry.
+    0x00_A_F_9B_000000_FFFF,
+    // Kernel data entry.
+    0x00_C_F_93_000000_FFFF,
+];
+
+/// Allocates and configures various mappings necessary to successfully boot.
+pub fn setup_general_mappings(
+    map: &mut VirtualMemoryMap,
+) -> Result<(u64, u64, u64), SetupMappingsError> {
+    let stack_frame_count = LOADED_STACK_SIZE.div_ceil(4096);
+    let stack = boot::allocate_pages(
+        AllocateType::AnyPages,
+        MemoryType::LOADER_DATA,
+        stack_frame_count as usize,
+    )
+    .map_err(|error| SetupMappingsError::StackAllocationFailed {
+        frame_count: stack_frame_count,
+        status: error.status(),
+    })?;
+    let stack_frames = FrameRange::new((stack.as_ptr() as u64) >> 12, stack_frame_count)
+        .expect("broken page allocator");
+    let stack_pages = generate_random_supervisor_base(map, stack_frames, true, false);
+    let _ = with_stdout(|stdout| writeln!(stdout, "Stack frames: {stack_frames:X?}"));
+
+    let gdt_context_switch =
+        boot::allocate_pages(AllocateType::AnyPages, MemoryType::LOADER_CODE, 1).map_err(
+            |error| SetupMappingsError::GdtContextSwitchAllocationFailed {
+                status: error.status(),
+            },
+        )?;
+    let gdt_context_switch_frames = FrameRange::new((gdt_context_switch.as_ptr() as u64) >> 12, 1)
+        .expect("broken page allocator");
+    map.insert(
+        VirtualMemoryMapEntry::new(
+            PageRange::new((gdt_context_switch.as_ptr() as u64) >> 12, 1).unwrap(),
+            gdt_context_switch_frames,
+            false,
+            true,
+        )
+        .expect("broken page allocator"),
+    )
+    .expect("GDT context switch mapping failed");
+
+    unsafe {
+        core::ptr::copy_nonoverlapping(
+            GDT.as_ptr().cast::<u8>(),
+            gdt_context_switch.as_ptr(),
+            mem::size_of_val(&GDT),
+        )
+    }
+    unsafe {
+        core::ptr::copy_nonoverlapping(
+            CONTEXT_STUB_BYTES.as_ptr().cast::<u8>(),
+            gdt_context_switch.as_ptr().add(mem::size_of_val(&GDT)),
+            mem::size_of_val(&CONTEXT_STUB_BYTES),
+        )
+    }
+
+    let _ = with_stdout(|stdout| {
+        writeln!(
+            stdout,
+            "GDT + context switch frames: {gdt_context_switch_frames:X?}"
+        )
+    });
+
+    Ok((
+        stack_pages.virtual_address() as u64,
+        gdt_context_switch.as_ptr() as u64,
+        gdt_context_switch.as_ptr() as u64 + mem::size_of_val(&GDT) as u64,
+    ))
+}
+
+/// Various errors that can occur while allocating general page mappings.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum SetupMappingsError {
+    /// The allocation of stack frames failed.
+    StackAllocationFailed {
+        /// The number of frames that the allocation requested.
+        frame_count: u64,
+        /// The status code returned from the failed allocation.
+        status: Status,
+    },
+    /// The allocation of the gdt and context frames failed.
+    GdtContextSwitchAllocationFailed {
+        /// The status code returned from the failed allocation.
+        status: Status,
+    },
+}
+
+impl fmt::Display for SetupMappingsError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::StackAllocationFailed {
+                frame_count,
+                status,
+            } => write!(
+                f,
+                "allocation of {frame_count} pages for the stack failed with code {status}"
+            ),
+            Self::GdtContextSwitchAllocationFailed { status } => write!(
+                f,
+                "allocation of 1 page for GDT and context switch failed with code {status}"
+            ),
+        }
+    }
+}
+
+/// Generates a random address in the supervisor section of memory, marks it as to be mapped as
+/// writable or executable according to the passed objects, and then puts in into the
+/// [`VirtualMemoryMap`].
+pub fn generate_random_supervisor_base(
+    map: &mut VirtualMemoryMap,
+    frames: FrameRange,
+    writable: bool,
+    executable: bool,
+) -> PageRange {
+    loop {
+        let rng = load_application::rand_u64().expect("random number generator failed")
+            & PageRange::PAGE_MASK;
+        let Some(pages) = PageRange::new(rng, frames.size()) else {
+            continue;
+        };
+
+        let Some(entry) = VirtualMemoryMapEntry::new(pages, frames, writable, executable) else {
+            continue;
+        };
+
+        if map.insert(entry).is_some() {
+            return pages;
+        }
+    }
+}
 
 #[cfg_attr(not(test), panic_handler)]
 #[cfg_attr(test, expect(dead_code))]
